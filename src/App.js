acab4ba82c1436f54eeeb7ea13fca70588f5c9b2
Juan Simon <113181422+Juansim0n@users.noreply.github.com>
2022-12-03 00:28:52 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="chefe">
+       <main>
+  <div>
+    |1| Chefe Claude Troisgros  
+  </div>
+  <div>
+    |2| Chefe Alain Ducasse    
+  </div>
+  <div>
+    |3| Chefe Jean Roca 
+  </div>
+  <div>
+    |4| Chefe Manu Buffara  
+  </div>
+</main>
+    </div>
+  );
+}
+
+
+export default App;
